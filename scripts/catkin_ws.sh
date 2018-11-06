d30144d1535f98410c8410edf87f4f7a9519f62f
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2018-11-06 11:49:09 +0000

    changed to master branch on our dense_flow package

--- scripts/catkin_ws.sh
+++ scripts/catkin_ws.sh
@@ -4,9 +4,9 @@ source /root/ros_catkin_ws/install_isolated/setup.bash
 mkdir -p $MYDIR/catkin_ws/src
 cd $MYDIR/catkin_ws/src
 git clone https://github.com/mysablehats/dense_flow.git --recursive # this won't be necessary since we will remove easylogging and libzip, but... https://github.com/frederico-klein/create_video.git
-cd dense_flow
-git checkout roslog
-cd ..
+#cd dense_flow
+#git checkout roslog
+#cd ..
 #git clone https://github.com/ros-perception/vision_opencv.git
 #latest version is too new. it requires opencv3 and since i don't want to install
 #2 versions or change the tsn code to use opencv3 we will use an older version of
